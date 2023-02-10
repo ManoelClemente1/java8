9ddf7c64761759ce0e85b994e4f8e91411cb494a
Manoel C <manoelclemente9@outlook.com>
2023-02-10 20:05:29 -0300

    first commit

---
+++ src/OrdenaString.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenaString {
+
+    public static void main(String[] args) {
+
+        List<String> palavras = new ArrayList<>();
+
+        palavras.add("alura online");
+        palavras.add("editora casa do código");
+        palavras.add("caelum");
+
+        Collections.sort(palavras);
+
+        System.out.println(palavras);
+
+    }
+}
