45e94e3b5fe1c8f02bfe3ed2742a1d5fd80fd74b
Manoel C <manoelclemente9@outlook.com>
2023-02-11 20:34:25 -0300

    add streams

--- src/OrdenaString.java
+++ src/OrdenaString.java
@@ -3,6 +3,8 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class OrdenaString {
 
@@ -17,22 +19,48 @@ public class OrdenaString {
 
         //Collections.sort(palavras,comparator);
 
-        palavras.sort((s1, s2) -> {
-            if(s1.length() < s2.length()){
-                return -1;
-            }
-            if(s1.length() > s2.length()){
-                return 1;
-            }
-
-            return 0;
-        });
+//        palavras.sort((s1, s2) -> {
+//            if(s1.length() < s2.length()){
+//                return -1;
+//            }
+//            if(s1.length() > s2.length()){
+//                return 1;
+//            }
+//
+//            return 0;
+//        });
 
         palavras.sort((s1, s2) -> Integer.compare(s1.length(),s2.length()));
+
+        palavras.sort(Comparator.comparing(s-> s.length()));
+        palavras.sort(Comparator.comparing(String::length));
+
+
+        Function<String, Integer> funcao = s -> s.length();
+        Comparator<String> comparador = Comparator.comparing(funcao);
+        palavras.sort(comparador);
 
         System.out.println(palavras);
 
         palavras.forEach(s -> System.out.println(s));
+
+//        System.out.println(palavras.stream().count());
+        System.out.println("##############");
+        System.out.println("####### filter curso caelum #######");
+        palavras.stream()
+                .filter(s -> s.equals("caelum"))
+                .forEach(System.out::println);
+
+        System.out.println("##############");
+        System.out.println("####### devolvendo resultado após filtrar #######");
+
+        List<String> teste = palavras.stream()
+                .filter(s -> s.equals("caelum"))
+                .collect(Collectors.toList());
+
+        teste.forEach(System.out::println);
+
+//        palavras.forEach(System.out::println);
 
 
     }
