677df9b550c5e4c34942657328e649821b31e123
Manoel C <manoelclemente9@outlook.com>
2023-02-11 09:52:26 -0300

    add anonymous class

--- src/OrdenaString.java
+++ src/OrdenaString.java
@@ -14,41 +14,26 @@ public class OrdenaString {
         palavras.add("editora casa do código");
         palavras.add("caelum");
 
-        Comparator<String> comparator = new ComparadorPorTamanho();
+
         //Collections.sort(palavras,comparator);
 
-        palavras.sort(comparator);
+        palavras.sort((s1, s2) -> {
+            if(s1.length() < s2.length()){
+                return -1;
+            }
+            if(s1.length() > s2.length()){
+                return 1;
+            }
+
+            return 0;
+        });
+
+        palavras.sort((s1, s2) -> Integer.compare(s1.length(),s2.length()));
 
         System.out.println(palavras);
 
-        Consumer<String> consumidor = new ImprimeNaLinha();
-        palavras.forEach(consumidor);
+        palavras.forEach(s -> System.out.println(s));
 
 
-    }
-}
-
-class ImprimeNaLinha implements  Consumer<String>{
-
-    @Override
-    public void accept(String s) {
-        System.out.println(s);
-    }
-}
-
-
-class ComparadorPorTamanho implements Comparator<String>{
-
-
-    @Override
-    public int compare(String s1, String s2) {
-        if(s1.length() < s2.length()){
-            return -1;
-        }
-        if(s1.length() > s2.length()){
-            return 1;
-        }
-
-        return 0;
     }
 }
